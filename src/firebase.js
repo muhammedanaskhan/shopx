ed77bebaf96ceba669409f235061163bdd1f5bd0
muhammedanaskhan <muhammedanas5186@gmail.com>
2022-12-22 09:35:47 -0800

    deleted unuseful files, clean base app, firebase config added!

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdKQgAZTSkgR_BVZwjkd6ZD1u7R28lhlE",
+    authDomain: "shopx-42892.firebaseapp.com",
+    projectId: "shopx-42892",
+    storageBucket: "shopx-42892.appspot.com",
+    messagingSenderId: "168039020574",
+    appId: "1:168039020574:web:1cdf9d0a48004cf43e160f"
+  };
