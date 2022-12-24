8205858e37263c7e76df36d87bc867889462c0f6
muhammedanaskhan <muhammedanas5186@gmail.com>
2022-12-24 04:58:41 -0800

    Router added, next the work will be on Checkout page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { Switch } from '@mui/material';
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-        {/*HEADER*/}
-        <Header/>
-        {/*HOME*/}
-        <Home></Home>
 
-    </div>
+    <Router>
+      <div className="app">
+
+        <Routes>
+          <Route path='/' element={[<Header />,<Home />]}/>
+          <Route path='/checkout' element={[<Header />,<h1>Checkout</h1>]}/>
+            
+
+          
+        </Routes>
+
+
+      </div>
+    </Router>
   );
 }
 
