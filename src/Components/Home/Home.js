1447a6f1cad44e6b5f7c220b355e351043da0567
muhammedanaskhan <muhammedanas5186@gmail.com>
2022-12-24 00:53:38 -0800

    Home page product rows styles, inflated with some raw data hardcoded

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css';
+import Product from '../Product';
 
 function Home() {
   return (
@@ -8,16 +9,16 @@ function Home() {
             <img className='home__image' src='/images/disnep_up.png'></img>
 
             <div className="home__row">
-                {/*Product*/}
-                {/*Product*/}
+                <Product title="The lean startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses" price={29.99} image={`/images/thelean__book.jpg`} rating={3}/>
+                <Product title='Rossmann Professional Stand Mixer, Powerful 1400 Watts, 100% Pure Copper Motor, 4 Safety Features' price={239} image={`/images/product2.jpg`} rating={5}/>
             </div>
             <div className="home__row">
-                {/*Product*/}
-                {/*Product*/}
-                {/*Product*/}
+                <Product title='EXZZER Latest ID116 Plus Bluetooth Smart Fitness Band Watch' price={29.99} image={`/images/watch.jpg`} rating={5}/>
+                <Product title='Echo (4th Gen, 2020 release) | Premium sound powered by Dolby and Alexa (Black)' price={29.99} image={`/images/echo.jpg`} rating={5}/>
+                <Product title='Apple 2022 11-inch iPad Pro (Wi-Fi + Cellular, 512GB) - Space Grey (4th Generation)' price={29.99} image={`/images/ipad.png`} rating={5}/>
             </div>
             <div className="home__row">
-                {/*Product*/}
+                <Product title='Samsung LC49RG90SSUXEN 49" Curved LED Gaming Monitor - Dual WQHD 5120 x 1440, 120Hz, HDMI, Displayport, USB, 120Hz refresh rate with AMD Free Sync 2 technology for crisp HDR content display, reduced input latency, low frame rate compensation.' price={29.99} image={`/images/led1.jpg`} rating={5}/>
             </div>
         </div>
     </div>
