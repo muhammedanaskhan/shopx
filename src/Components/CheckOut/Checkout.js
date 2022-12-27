3a907ed2baa2290799d9f7a5d80d4b534ecc8893
muhammedanaskhan <muhammedanas5186@gmail.com>
2022-12-27 07:38:54 -0800

    fetching Login E-mail & displaying on Checkout page

--- src/Components/CheckOut/Checkout.js
+++ src/Components/CheckOut/Checkout.js
@@ -6,13 +6,14 @@ import Subtotal from '../Subtotal/Subtotal';
 
 function Checkout() {
 
-  const[{basket}, dispatch] = useStateValue();
+  const[{basket, user}, dispatch] = useStateValue();
 
   return (
     <div className='checkout'>
         <div className="checkout__left">
             <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
             <div className="">
+              <h3>{user?.email}</h3>
                 <h2 className="checkout__title">Your shopping basket</h2>
                 
                 {basket.map(item => (
